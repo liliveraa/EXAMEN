bca2114609a3adf0e9383e2e5355914c972921cc
liliveraa <lilimay.150507@gmail.com>
2025-04-08 05:18:53 +0200

    Add files via upload

---
+++ lilian.js
@@ -0,0 +1,11 @@
+function saludar()
+{
+    let nombre= prompt("Ingrese su nombre");
+    alert("Hola "+nombre+", suerte en tu examen");
+}
+function calcularCuadrado()
+{
+    let numero=parseFloat(prompt("Ingrese un número"));
+    let cuadrado=numero*numero;
+    alert ("El cuadrado del número es "+cuadrado);
+}
